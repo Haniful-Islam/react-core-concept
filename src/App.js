9375fe5aed92b3fdd32d704642380a5040b47234
Haniful-Islam <haniful360@gmail.com>
2022-08-12 18:19:52 +0600

    react core concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const products = [
+    {name:'iPhone', price:150000, color:'golden'},
+    {name:'Samsung', price:30000, color:'blue'},
+    {name:'watch', price:3500, color:'black'},
+    {name:'Vivo', price:150000, color:'made black'},
+  ]
+  return(
+    <div className='App'>
+      {
+        products.map(product => <Product name={product.name} price={product.price} color={product.color}></Product>)
+      }
+      {/* <Product name='Phone' price="50000"></Product>
+      <Product name='laptop' price="100000"></Product>
+      <Product name='watch' price="1000"></Product> */}
     </div>
-  );
+  )
+}
+
+function Product(props) {
+  console.log(props);
+  return(
+    <div className='product'>
+      <h2>Name: {props.name}</h2>
+      <h3>Price :{props.price}</h3>
+      <h3>Price :{props.color}</h3>
+    </div>
+  )
 }
 
 export default App;
